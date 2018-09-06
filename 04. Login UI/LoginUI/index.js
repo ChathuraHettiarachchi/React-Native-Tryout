0411b60caa2174d41d945be1c7adc96492efa1a1
Chathura Hettiarachchi <chathura93@yahoo.com>
2018-09-06 15:03:07 +0530

    - add splash to new activity

--- 04. Login UI/LoginUI/index.js
+++ 04. Login UI/LoginUI/index.js
@@ -1,10 +1,28 @@
 /** @format */
 
+import React, { Component } from 'react'
 import {AppRegistry} from 'react-native';
+
 import App from './App';
 import Splash from './components/Splash';
 import Login from './components/Login';
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {currentScreen: 'Splash'}
+
+        setTimeout(() => {
+            this.setState({currentScreen: 'Login'})
+        }, 3000)
+    }
+    render() {
+        const { currentScreen } = this.state
+        let mainScreen = currentScreen === 'Splash' ? <Splash/> : <Login/>
+        return mainScreen
+    }
+}
+
+AppRegistry.registerComponent(appName, () => Main);
